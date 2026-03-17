bf25278ae3f8c4df2bef5a021ccf43a8cb29c0fd
Artur P.C. Amaral <artur.portuguez@gmail.com>
2026-03-17 16:35:26 -0300

    Estrutura completa com postgres e docker

--- config/settings.py
+++ config/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "drf_api",
+        "USER": "postgres",
+        "PASSWORD": "rV4@94NZWy3&3W&", 
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
