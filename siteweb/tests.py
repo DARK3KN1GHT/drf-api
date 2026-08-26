23656bff46a653a9f90b5d4d6c6cfbad59e0652e
Artur P.C. Amaral <artur.portuguez@gmail.com>
2026-08-26 15:24:01 -0300

    Adiciona teste da pagina inicial

--- siteweb/tests.py
+++ siteweb/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, override_settings
 
-# Create your tests here.
+
+TEST_STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
+
+@override_settings(STORAGES=TEST_STORAGES)
+class HomePageTests(SimpleTestCase):
+
+    def test_home_page_retorna_status_200(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
